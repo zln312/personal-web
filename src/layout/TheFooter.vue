53c80dbaa7ea8db0d9dd4c3b04a12f755ab7cef7
zln <714323486@qq.com>
2021-02-22 19:28:38 +0800

    a

--- src/layout/TheFooter.vue
+++ src/layout/TheFooter.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-21 19:27:13
- * @LastEditTime: 2021-02-21 21:24:38
+ * @LastEditTime: 2021-02-22 19:27:23
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \personal\src\layout\Thefooting.vue
@@ -11,7 +11,7 @@
     <div>
       <span class="text">Copyright ©2019 zlnkyb.cn All Rights Reserved | </span>
       <a href="https://beian.miit.gov.cn/">
-        陕ICP备19025101号
+        陕ICP备19025101号-1
       </a>
     </div>
   </div>
